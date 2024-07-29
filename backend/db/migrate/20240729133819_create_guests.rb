2c66e26b36176a9c99a061fb70e2c0e030c1c2b1
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-29 10:02:28 -0400

    Complete models and migrations

--- backend/db/migrate/20240729133819_create_guests.rb
+++ backend/db/migrate/20240729133819_create_guests.rb
@@ -1,4 +1,10 @@
 class CreateGuests < ActiveRecord::Migration[6.1]
   def change
+    create_table :guests do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
   end
 end
