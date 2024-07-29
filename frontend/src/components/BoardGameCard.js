4cb8c9ae2a1db9d699c972ea0e9774dc4491b762
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-29 15:51:46 -0400

    Display board games in a grid on page

--- frontend/src/components/BoardGameCard.js
+++ frontend/src/components/BoardGameCard.js
@@ -1,22 +1,14 @@
 
 function BoardGameCard({ boardGame }) {
   return (
-    // <div className="col-lg-4 mb-4">
-    //   <div className="card mx-auto" style={{ width: "25rem" }}>
-    //     <div className="card-body">
-    //       <h5 className="card-title">{boardGame.name}</h5>
-    //       <p>
-    //         {/* <Link to={`/spells/${boardGame.id}`}>View Board Game</Link> */}
-    //       </p>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="card">
-      <img src={boardGame.image} className="card-img-top" alt={boardGame.name}/>
-      <div className="card-body">
-        <h5 className="card-title">{boardGame.name}</h5>
-        <p className="card-text">{boardGame.description}</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="col">
+      <div className="card" style={{width:'20rem'}}>
+        <img src={boardGame.image} className="card-img-top" alt={boardGame.name}/>
+        <div className="card-body">
+          <h5 className="card-title">{boardGame.name}</h5>
+          <p className="card-text">{boardGame.description}</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     </div>
   )
