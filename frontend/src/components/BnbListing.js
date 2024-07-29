8eac1791caf992ad37b92520a755290a3b530130
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-29 15:38:39 -0400

    Test displaying guest log entries in console on click

--- frontend/src/components/BnbListing.js
+++ frontend/src/components/BnbListing.js
@@ -1,7 +1,25 @@
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 function BnbListing({ bnb }) {
-  const { name, location, num_of_rooms, cost_per_night, description } = bnb
+  const { id, name, location, num_of_rooms, cost_per_night, description } = bnb
+
+  const [guestLog, setGuestLog] = useState([])
+
+  // function getGuestLog() {
+  //   console.log(id)
+
+  //   fetch(`http://localhost:9292//bnbs/${id}/guest_log`)
+  //     .then((r) => r.json())
+  //     .then((entries) => console.log(entries))
+  // }
+
+  const getGuestLog = () => {
+    return fetch(`http://localhost:9292//bnbs/${id}/guest_log`)
+      .then((r) => r.json())
+      .then((entries) => console.log(entries))
+  }
+
   return (
     <div>
       <h2>{name}</h2>
@@ -10,7 +28,7 @@ function BnbListing({ bnb }) {
       </span>
       <p>{description}</p>
       <p>Located in {location}</p>
-      <button>
+      <button onClick={getGuestLog}>
         <Link to="/guest_log">View Guest Log</Link>
       </button>
       <button>
