b91aae562c39dff28fd8907de18953db5bc36954
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-29 10:57:51 -0400

    Update model relationships

--- backend/app/models/board_game.rb
+++ backend/app/models/board_game.rb
@@ -1,6 +1,6 @@
 class BoardGame < ActiveRecord::Base
   has_many :reviews
-  has_many :collection_board_game
+  has_many :collection_board_games
   has_many :users, through: :reviews
-  has_many :collections, through: :collection_board_game
+  has_many :collections, through: :collection_board_games
 end
--- backend/app/models/collection.rb
+++ backend/app/models/collection.rb
@@ -1,5 +1,5 @@
 class Collection < ActiveRecord::Base
   belongs_to :user
-  has_many :collection_board_game
-  has_many :board_games, through: :collection_board_game
+  has_many :collection_board_games
+  has_many :board_games, through: :collection_board_games
 end
