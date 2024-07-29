30a568cc927233b27326416065a9a306ccce33e7
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-29 16:28:21 -0400

    Added methods to post shoes, new runners, and get runners.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,10 +1,48 @@
-# frozen_string_literal: true
-
 class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
-  # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    "Hello World"
+  end
+
+  get "/runners" do
+    runners = Runner.all.to_a.map do |runner|
+      {
+        id: runner.id,
+        name: runner.name,
+        shoes: runner.shoes,
+      }
+    end
+    runners.to_json
+  end
+
+  post "/shoes" do
+    shoe = Shoe.create(
+      name: params[:name],
+      mileage: 0,
+      runner_id: params[:runnerId]
+    )
+    shoe.to_json
+  end
+
+  post "/runners" do
+    runner = Runner.create(
+      name: params[:name]
+    )
+    runner.to_json
+  end
+
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
   end
 end
