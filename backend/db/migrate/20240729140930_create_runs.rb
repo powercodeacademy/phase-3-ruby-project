c43944b685917036898abf0a20afe7d1b0d8420c
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-29 10:28:36 -0400

    Fix runs migration error

--- backend/db/migrate/20240729140930_create_runs.rb
+++ backend/db/migrate/20240729140930_create_runs.rb
@@ -2,7 +2,7 @@ class CreateRuns < ActiveRecord::Migration[6.1]
   def change
     create_table :runs do |t|
       t.references :runner, foreign_key: true
-      t.references :sneaker, foreign_key: true
+      t.references :shoe, foreign_key: true
       t.integer :distance
 
       t.timestamps
---
+++ backend/db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2024_07_29_140937) do
+
+  create_table "runners", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "runs", force: :cascade do |t|
+    t.integer "runner_id"
+    t.integer "shoe_id"
+    t.integer "distance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["runner_id"], name: "index_runs_on_runner_id"
+    t.index ["shoe_id"], name: "index_runs_on_shoe_id"
+  end
+
+  create_table "shoes", force: :cascade do |t|
+    t.integer "runner_id"
+    t.string "name"
+    t.integer "mileage"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["runner_id"], name: "index_shoes_on_runner_id"
+  end
+
+  add_foreign_key "runs", "runners"
+  add_foreign_key "runs", "shoes"
+  add_foreign_key "shoes", "runners"
+end
