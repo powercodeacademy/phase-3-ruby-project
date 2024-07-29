cc5b7329ac7e6a68b5f2c14e4a1609830e962ba9
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-29 10:59:26 -0400

    Update route to /board_games

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
   # Add your routes here
-  get "/" do
+  get "/board_games" do
     { message: "Good luck with your project!" }.to_json
   end
 end
