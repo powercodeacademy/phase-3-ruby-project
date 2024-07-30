8b5b747d71326db424199c35e1a2fff7b5539ade
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-30 10:07:54 -0400

    Add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 import ToolsList from "./components/ToolList"
+import Header from "./components/Header"
 
 const App = () => {
   return (
     <div>
+      <Header />
       <main>
         <ToolsList />
       </main>
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>
+        Lumpy Tool Rental
+        <span className="logo" role="img">
+          🛠️
+        </span>
+      </h1>
+    </header>
+  )
+}
+
+export default Header
