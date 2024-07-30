f3af8ee255c8d3a1ca9d29f200cad61abf6033a7
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-30 14:42:22 -0400

    Create login fetches for login page

--- frontend/src/services/fetchers.js
+++ frontend/src/services/fetchers.js
@@ -1,4 +1,6 @@
 const urlBoardGames = "http://localhost:9292/board_games/"
+const urlAuth = "http://localhost:9292/current_user"
+const urlLogin = "http://localhost:9292/login"
 
 const getAllBoardGames = () => {
   return fetch(urlBoardGames).then(resp => resp.json())
@@ -8,4 +10,16 @@ const getBoardGameData = (id) => {
   return fetch(urlBoardGames + id).then(resp => resp.json())
 }
 
-export { getAllBoardGames, getBoardGameData }
+const loginUser = (username, password) => {
+  return fetch(urlLogin, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password }),
+  }).then(resp => resp.json())
+}
+
+const authCheck = () => {
+  return fetch(urlAuth).then(resp => resp.json())
+}
+
+export { getAllBoardGames, getBoardGameData, loginUser, authCheck }
