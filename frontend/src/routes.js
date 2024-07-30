7c3e5915af3d45089f37e5e00dbbe570d6a0c0b8
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-30 09:23:33 -0400

    Update routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,14 @@
 import App from "./App";
+import BoardGameDetailsPage from "./Pages/BoardGameDetailsPage";
 
 const routes = [
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/board_games/:index",
+    element: <BoardGameDetailsPage />
   }
 ]
 
