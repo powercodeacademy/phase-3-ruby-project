125a1930adf493fb96c2d7273373958634bb0432
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-30 16:25:40 -0400

    Add password column to users table and update seed data

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_729_134_244) do
+ActiveRecord::Schema.define(version: 2024_07_30_184829) do
+
   create_table "board_games", force: :cascade do |t|
     t.string "name"
     t.string "image"
@@ -51,5 +52,7 @@ ActiveRecord::Schema.define(version: 20_240_729_134_244) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
+
 end
