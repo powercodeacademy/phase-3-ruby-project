b5f2acbbd0979f12f0e0c36837270f64f8ace4da
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-30 10:48:55 -0400

    Add form and POST for GuestLog

--- frontend/src/components/GuestLogForm.js
+++ frontend/src/components/GuestLogForm.js
@@ -1,11 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-function GuestLogForm() {
+function GuestLogForm({ entry }) {
+  const { bnb_id, check_in, guest_id, id } = entry
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newEntry = {
+      entry_date: check_in,
+      message: message,
+      bnb_id: bnb_id,
+      guest_id: guest_id,
+      stay_id: id,
+    }
+
+    fetch(`http://localhost:9292/guest_log`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newEntry),
+    })
+      .then((r) => r.json())
+      .then(setMessage(""))
+  }
+
   return (
-    <div>
-      <h3>Sign the guest log.</h3>
-    </div>
+    <form onSubmit={handleSubmit} className="text-center">
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          className="form-control"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="button-74">
+        Add Entry
+      </button>
+    </form>
   )
 }
 
--- frontend/src/components/StayEntry.js
+++ frontend/src/components/StayEntry.js
@@ -1,13 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
+import GuestLogForm from "./GuestLogForm"
 
 function StayEntry({ entry }) {
+  const [guestLogForm, setGuestLogForm] = useState(false)
   const { check_in, check_out, guest } = entry
+
+  const toggleGuestLogForm = () => {
+    setGuestLogForm(!guestLogForm)
+  }
+
   return (
     <tr>
       <td>{check_in}</td>
       <td>{check_out}</td>
       <td>{guest.name}</td>
+      <td>
+        <button className="button-74" onClick={toggleGuestLogForm}>
+          {guestLogForm ? "Hide Guest Log" : "Sign Guest Log"}
+        </button>
+        {guestLogForm && <GuestLogForm entry={entry} />}
+      </td>
     </tr>
   )
 }
