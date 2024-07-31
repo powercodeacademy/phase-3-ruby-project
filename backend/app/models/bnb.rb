b5ca026dadb42d7ef02b11ca3900b3f6d54ee6f9
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-31 09:27:30 -0400

    Add total revenue instance method

--- backend/app/models/bnb.rb
+++ backend/app/models/bnb.rb
@@ -2,4 +2,8 @@ class Bnb < ActiveRecord::Base
   has_many :stays
   has_many :guests, through: :stays
   has_many :guest_log_entries, through: :stays
+
+  def total_revenue
+    cost_per_night * stays.count
+  end
 end
