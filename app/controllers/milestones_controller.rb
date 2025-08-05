5855e6b75b96ddba01d83d4efa12e67ecbbd4f90
Kyle <kylegranthousel@gmail.com>
2025-08-05 10:03:37 -0400

    Make milestone methods

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -63,5 +63,26 @@ class MilestonesController < ApplicationController
   end
 
   post "/milestones/:milestone_id/entries" do
+    milestone = Milestone.find_by(id: params[:milestone_id])
+    if milestone.nil?
+      status 404
+      return { error: "Milestone not found" }.to_json
+    end
+
+    child = Child.find_by(id: params[:child_id])
+    if child.nil?
+      status 404
+      return { error: "Child not found" }.to_json
+    end
+
+    entry = Entry.create(
+      child_id: child.id,
+      milestone_id: milestone.id,
+      note: params[:note],
+      date: Date.parse(params[:date]),
+      age_months: params[:age_months]
+    )
+
+    entry.to_json(include: %i[child milestone])
   end
 end
