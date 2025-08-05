c576ed9ab74559bf0952bc23ad52536ecac46edb
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-05 10:35:18 -0400

    Sort receipts by descending date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
   get "/receipts" do
-    receipts = Receipt.all.order(:date)
+    receipts = Receipt.all.order(date: :desc)
     receipts.to_json(only: [:date], include: { store: { only: [:name] } })
   end
 end
