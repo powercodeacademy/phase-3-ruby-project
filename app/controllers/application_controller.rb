105916b2d4a77972f8a464ed0dae609cc1d5294c
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-05 10:33:42 -0400

    Get all receipts and their stores

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/receipts" do
+    receipts = Receipt.all.order(:date)
+    receipts.to_json(only: [:date], include: { store: { only: [:name] } })
   end
 end
