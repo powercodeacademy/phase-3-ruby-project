6108230b303c1928cdf49371606f66dfb173aedc
Kyle <kylegranthousel@gmail.com>
2025-08-05 08:49:49 -0400

    Create child method

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -7,6 +7,19 @@ class ChildrenController < ApplicationController
   end
 
   post "/children" do
+    begin
+      parsed_birthdate = Date.parse(params[:birthdate])
+    rescue ArgumentError
+      status 400
+      return { error: "Invalid birthdate format. Use YYYY-MM-DD." }.to_json
+    end
+
+    new_child = Child.create(
+      name: params[:name],
+      birthdate: parsed_birthdate
+    )
+
+    new_child.to_json
   end
 
   patch "/children/:id" do
